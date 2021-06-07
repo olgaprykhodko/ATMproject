2aec61ae07ed979bb0637cb1798f0bc593e371b3
svetagonchar <svetlana.gonchar.2012@gmail.com>
2021-06-08 00:29:45 +0300

    Some changes

--- src/main/java/com/solvd/ATMproject/main/Main.java
+++ src/main/java/com/solvd/ATMproject/main/Main.java
@@ -13,11 +13,15 @@ public class Main {
         log.info("Welcome to BSB bank");
 
         AddressService.getAddress("Mira");
+        log.info("You choose the ATM on Mira street");
 
         CardService.getStatusCardByNumberCard("1234123412341234");
+        log.info("Your card is active");
 
         ATMService.getCommissionByCurrency("dollars");
+        log.info("You choose the currency - dollars");
 
         BalanceService.getCardBalance(800);
+        log.info("Your balance is 800");
     }
 }
