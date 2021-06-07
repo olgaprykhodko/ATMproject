49d86b856ea6f6c715dd9cbf9b55102668b28bca
svetagonchar <svetlana.gonchar.2012@gmail.com>
2021-06-08 00:23:57 +0300

    Some changes

--- src/main/java/com/solvd/ATMproject/main/Main.java
+++ src/main/java/com/solvd/ATMproject/main/Main.java
@@ -10,6 +10,8 @@ import lombok.extern.log4j.Log4j2;
 public class Main {
     public static void main(String[] args) {
 
+        log.info("Welcome to BSB bank");
+
         AddressService.getAddress("Mira");
 
         CardService.getStatusCardByNumberCard("1234123412341234");
