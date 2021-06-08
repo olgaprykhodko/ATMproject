56f64128b804648d96207aaa2e1d45a187cbec56
svetagonchar <svetlana.gonchar.2012@gmail.com>
2021-06-08 03:01:33 +0300

    Some changes

--- src/main/java/com/solvd/ATMproject/dao/jdbc/realization/CollectionTimeDAO.java
+++ src/main/java/com/solvd/ATMproject/dao/jdbc/realization/CollectionTimeDAO.java
@@ -1,6 +1,6 @@
 package com.solvd.ATMproject.dao.jdbc.realization;
 
-import com.solvd.ATMproject.dao.abstractClasses.AbstractJDBSDao;
+import com.solvd.ATMproject.dao.abstractClasses.AbstractJDBCDao;
 import com.solvd.ATMproject.dao.interfaces.IDateDAO;
 import com.solvd.ATMproject.models.CashCollection;
 import lombok.extern.log4j.Log4j2;
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.Date;
 
 @Log4j2
-public class CollectionTimeDAO extends AbstractJDBSDao implements IDateDAO {
+public class CollectionTimeDAO extends AbstractJDBCDao implements IDateDAO {
     private String GET_TIME = "SELECT Schedule FROM Cash_Collection WHERE Schedule = ?";
 
     @Override
--- src/main/java/com/solvd/ATMproject/service/AddressService.java
+++ src/main/java/com/solvd/ATMproject/service/AddressService.java
@@ -6,7 +6,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class AddressService {
-    public static void getAddress(String street)  {
+    public static void getAddress(String street) {
         IAddressDAO addressDAO = new AddressDAO();
         addressDAO.read(street);
     }
